cc79d3872aa1e1cc91560b504b68eac494078082
Subesh Yadav <subeshyadav3@gmail.com>
2024-12-04 20:17:51 +0545

    .

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import preact from '@preact/preset-vite';
 
 export default defineConfig({
   plugins: [preact()],
-  base: '/weather-app/',  // Ensures correct path handling for GitHub Pages
+  base: '/weather/',  // Ensures correct path handling for GitHub Pages
   css: {
     postcss: './postcss.config.js',  // Uses your custom PostCSS configuration
   },
