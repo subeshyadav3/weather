580407eb3a4486a7e0263edfa98c81e11be94530
Subesh Yadav <subeshyadav3@gmail.com>
2024-12-04 19:52:26 +0545

    .

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite';
 import preact from '@preact/preset-vite';
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [preact()],
+  base:"/weather-app/",
   css: {
     postcss: './postcss.config.js',
   },
   build: {
-    outDir: 'build',  // Change this to 'build' if you want the build folder to be named 'build'
+    outDir: 'build',  
   },
 });
